118a290f4023b71e95d57aa9ace3213092292627
Anonymous Applicant <anonymous@example.com>
2017-03-27 01:53:44 -0400

    get rid of the example endpoint

--- src/clj/organizer/endpoint/example.clj
+++
@@ -1,8 +0,0 @@
-(ns organizer.endpoint.example
-  (:require [compojure.core :refer :all]
-            [clojure.java.io :as io]))
-
-(defn example-endpoint [{{db :spec} :db}]
-  (context "/example" []
-    (GET "/" []
-      (io/resource "organizer/endpoint/example/example.html"))))
